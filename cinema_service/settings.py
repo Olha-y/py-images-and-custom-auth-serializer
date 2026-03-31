46024786649d8a7c537ec0545c722e0102e78952
Olha Yukhnenko <pyolh36@gmail.com>
2026-03-31 17:26:21 +0300

    fix too long string in settings.py

--- cinema_service/settings.py
+++ cinema_service/settings.py
@@ -141,6 +141,8 @@ MEDIA_URL = "/media/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 REST_FRAMEWORK = {
-    "DEFAULT_AUTHENTICATION_CLASSES": ["rest_framework.authentication.TokenAuthentication"],
-    "DEFAULT_PERMISSION_CLASSES": ["cinema.permissions.IsAdminOrIfAuthenticatedReadOnly"]
+    "DEFAULT_AUTHENTICATION_CLASSES":
+        ["rest_framework.authentication.TokenAuthentication"],
+    "DEFAULT_PERMISSION_CLASSES":
+        ["cinema.permissions.IsAdminOrIfAuthenticatedReadOnly"]
 }
